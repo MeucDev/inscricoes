5a58b2a003a1b4f34003661a1cbca8b655c9e238
Leandro Salvatti Piscke <piscke@gmail.com>
2017-10-30 22:01:45 -0200

    Ajustando inscricoes

--- database/seeds/AjustandoInscricoes.php
+++ database/seeds/AjustandoInscricoes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Pessoa;
-use App\Inscricoe;
+use App\Inscricoess;
 
 
 class AjustandoInscricoes extends Seeder
@@ -17,16 +17,37 @@ class AjustandoInscricoes extends Seeder
         $pessoas = App\Pessoa::all();
         
         foreach ($pessoas as $pessoa) {
-            if (App\Inscricoe::where('responsavel_id', $pessoa->id)->count() == 0)
+            if (!App\Inscricoes::where('pessoa_id', $pessoa->id)->exists())
                 continue;
 
-            $inscricao = App\Inscricoe::where('responsavel_id', $pessoa->id)->first();
+            $inscricao = App\Inscricoes::where('pessoa_id', $pessoa->id)->first();
 
             $inscricao->alojamento = $pessoa->alojamento;
             $inscricao->equipeRefeicao = $pessoa->equipeRefeicao;
             $inscricao->refeicao = $pessoa->refeicao;
             $inscricao->presencaConfirmada = $pessoa->presencaConfirmada;
             $inscricao->save();
+
+            $dependentes = $pessoa->dependentes;
+            foreach ($dependentes as $dependente){
+                if (App\Inscricoes::where('pessoa_id', $dependente->id)->exists())
+                    continue;
+
+                $inscricaoDependente = new App\Inscricoes;
+                $inscricaoDependente->numero_inscricao_responsavel = $inscricao->id;
+                $inscricaoDependente->dataInscricao = $inscricao->dataInscricao;
+                $inscricaoDependente->ano = $inscricao->ano;
+                $inscricaoDependente->inscricaoPaga = $inscricao->inscricaoPaga;
+                $inscricaoDependente->tipoInscricao = $inscricao->tipoInscricao;
+                
+                $inscricaoDependente->pessoa_id = $dependente->id;
+                $inscricaoDependente->alojamento = $dependente->alojamento;
+                $inscricaoDependente->equipeRefeicao = $dependente->equipeRefeicao;
+                $inscricaoDependente->refeicao = $dependente->refeicao;
+                $inscricaoDependente->presencaConfirmada = $dependente->presencaConfirmada;
+                $inscricaoDependente->save();
+                    
+            }
         }
     }
 }
