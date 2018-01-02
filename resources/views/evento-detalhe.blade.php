9ad343f36bad3a62aad704f41991aa1216bf8477
Matias G Henschel <matiashenschel@hotmail.com>
2018-01-02 21:07:19 -0200

    Revitalização da interface de cadastro

--- resources/views/evento-detalhe.blade.php
+++ resources/views/evento-detalhe.blade.php
@@ -16,6 +16,24 @@
 		<!-- Theme style -->
 		<link href="{{ asset("vendor/crudbooster/assets/adminlte/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />    
 		<link href="{{ asset("vendor/crudbooster/assets/adminlte/dist/css/skins/_all-skins.min.css")}}" rel="stylesheet" type="text/css" />
+		
+		<style media="all">
+			body {
+				background: #eee;
+			}
+			
+			.jumbotron {
+				background: white;
+			}
+			
+			.progress {
+				background: #ddd;
+			}
+			
+			.box > .person + .person > .box-header {
+				border-top: 2px solid #e7e7e7;
+			}
+		</style>
 	</head>
 	<body>
 		<div class="jumbotron">
@@ -26,117 +44,232 @@
 			</div>
 		</div>
 		<div class="container">
-			<div class="progress">
-				<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%">
+			<div class="progress progress-sm active">
+				<div class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%">
 					<span class="sr-only">60%</span>
 				</div>
 			</div>
 			<form role="form">
-				<div id="step_1" data-step="1" class="row step">
-					<h4>Dados do responsável</h4>
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="cpf">CPF</label>
-							<input type="text" class="form-control" id="cpf">
+				<div id="step_1" data-step="1" class="step">
+					<div class="box box-primary">
+						<div class="box-header with-border">
+							<h4 class="box-title">Dados do responsável</h4>
+						</div>
+						<div class="row box-body">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="cpf">CPF</label>
+									<input type="text" class="form-control" id="cpf">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="email">Email</label>
+									<input type="email" class="form-control" id="email">
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="email">Email</label>
-							<input type="email" class="form-control" id="email">
-						</div>
-					</div>
-					<div class="col-md-12 text-right">
-						<button type="submit" class="btn btn-primary">Iniciar</button>
+					<div class="text-right">
+						<button type="button" class="btn btn-primary">Iniciar</button>
 					</div>
 				</div>
-				<div id="step_2" data-step="2" class="row step">
-					<h4>Dados individuais</h4>
-					<ul class="list-group">
-						<li class="list-group-item">
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="nome">Nome</label>
-									<input type="text" class="form-control" id="nome">
+				<div id="step_2" data-step="2" class="step">
+					<div class="box box-primary">
+						<div class="box-header with-border">
+							<h4 class="box-title">Dados individuais</h4>
+						</div>
+						<div class="person">
+							<div class="box-header with-border">
+								<span class="badge bg-light-blue">1</span>
+								<span class="badge bg-green">Responsável</span>
+								<div class="box-tools pull-right">
+									<button type="button" class="btn btn-box-tool"><i class="fa fa-times"></i></button>
 								</div>
 							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="cpf">CPF</label>
-									<input type="text" class="form-control" id="cpf">
+							<div class="row box-body">
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="nome">Nome</label>
+										<input type="text" class="form-control" id="nome">
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="cpf">CPF</label>
+										<input type="text" class="form-control" id="cpf">
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="datanascimento">Data de Nascimento</label>
+										<input type="text" class="form-control" id="datanascimento" placeholder="dd/mm/aaaa">
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="sexo">Sexo</label>
+										<select name="alimentacao" id="alimentacao" class="form-control">
+											<option value="m">Masculino</option>
+											<option value="f">Feminino</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-md-5">
+									<div class="form-group">
+										<label for="email">Email</label>
+										<input type="email" class="form-control" id="email">
+									</div>
+								</div>
+								<div class="col-md-1">
+									<div class="form-group">
+										<label for="ddd">DDD</label>
+										<input type="ddd" class="form-control" id="ddd" maxlength="2">
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="telefone">Telefone</label>
+										<input type="text" class="form-control" id="telefone">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="hospedagem">Hospedagem</label>
+										<select name="hospedagem" id="hospedagem" class="form-control">
+											<option value="0">Camping</option>
+											<option value="1">Lar Filadélfia</option>
+											<option value="2">Outro / Hotel na cidade</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="alimentacao">Alimentação</label>
+										<select name="alimentacao" id="alimentacao" class="form-control">
+											<option value="0">Quiosque</option>
+											<option value="1">Lar Filadélfia</option>
+											<option value="2">Outro / Não necessito</option>
+										</select>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+						<div class="person">
+							<div class="box-header with-border">
+								<span class="badge bg-light-blue">2</span>
+								<div class="box-tools pull-right">
+									<button type="button" class="btn btn-box-tool"><i class="fa fa-times"></i></button>
 								</div>
 							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="email">Email</label>
-									<input type="email" class="form-control" id="email">
+							<div class="row box-body">
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="nome">Nome</label>
+										<input type="text" class="form-control" id="nome">
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="parentesco">Parentesco</label>
+										<select name="parentesco" id="parentesco" class="form-control">
+											<option value="0">Cônjuge</option>
+											<option value="1">Filho(a)</option>
+											<option value="2">Pai/Mãe</option>
+											<option value="3">Outro</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="datanascimento">Data de Nascimento</label>
+										<input type="text" class="form-control" id="datanascimento" placeholder="dd/mm/aaaa">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="hospedagem">Hospedagem</label>
+										<select name="hospedagem" id="hospedagem" class="form-control">
+											<option value="0">Camping</option>
+											<option value="1">Lar Filadélfia</option>
+											<option value="2">Outro / Hotel na cidade</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="alimentacao">Alimentação</label>
+										<select name="alimentacao" id="alimentacao" class="form-control">
+											<option value="0">Quiosque</option>
+											<option value="1">Lar Filadélfia</option>
+											<option value="2">Outro / Não necessito</option>
+										</select>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+						<div class="person">
+							<div class="box-header with-border">
+								<span class="badge bg-light-blue">3</span>
+								<div class="box-tools pull-right">
+									<button type="button" class="btn btn-box-tool"><i class="fa fa-times"></i></button>
 								</div>
 							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="telefone">Telefone</label>
-									<input type="text" class="form-control" id="telefone">
+							<div class="row box-body">
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="nome">Nome</label>
+										<input type="text" class="form-control" id="nome">
+									</div>
 								</div>
-							</div>
-							<div class="clearfix"></div>
-						</li>
-						<li class="list-group-item">
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="nome">Nome</label>
-									<input type="text" class="form-control" id="nome">
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="parentesco">Parentesco</label>
+										<select name="parentesco" id="parentesco" class="form-control">
+											<option value="0">Cônjuge</option>
+											<option value="1">Filho(a)</option>
+											<option value="2">Pai/Mãe</option>
+											<option value="3">Outro</option>
+										</select>
+									</div>
 								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="cpf">CPF</label>
-									<input type="text" class="form-control" id="cpf">
+								<div class="col-md-3">
+									<div class="form-group">
+										<label for="datanascimento">Data de Nascimento</label>
+										<input type="text" class="form-control" id="datanascimento" placeholder="dd/mm/aaaa">
+									</div>
 								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="email">Email</label>
-									<input type="email" class="form-control" id="email">
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="hospedagem">Hospedagem</label>
+										<select name="hospedagem" id="hospedagem" class="form-control">
+											<option value="0">Camping</option>
+											<option value="1">Lar Filadélfia</option>
+											<option value="2">Outro / Hotel na cidade</option>
+										</select>
+									</div>
 								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="telefone">Telefone</label>
-									<input type="text" class="form-control" id="telefone">
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="alimentacao">Alimentação</label>
+										<select name="alimentacao" id="alimentacao" class="form-control">
+											<option value="0">Quiosque</option>
+											<option value="1">Lar Filadélfia</option>
+											<option value="2">Outro / Não necessito</option>
+										</select>
+									</div>
 								</div>
+								<div class="clearfix"></div>
 							</div>
-							<div class="clearfix"></div>
-						</li>
-						<li class="list-group-item">
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="nome">Nome</label>
-									<input type="text" class="form-control" id="nome">
-								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="cpf">CPF</label>
-									<input type="text" class="form-control" id="cpf">
-								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="email">Email</label>
-									<input type="email" class="form-control" id="email">
-								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="form-group">
-									<label for="telefone">Telefone</label>
-									<input type="text" class="form-control" id="telefone">
-								</div>
-							</div>
-							<div class="clearfix"></div>
-						</li>
-					</ul>
-					<div class="col-md-12 text-right">
-						<button type="submit" class="btn btn-primary">Cadastrar</button>
+						</div>
+						<div class="box-footer text-right">
+							<button type="button" class="btn btn-success">Adicionar</button>
+						</div>
+					</div>
+					<div class="text-right">
+						<button type="button" class="btn btn-primary">Confirmar</button>
 					</div>
 				</div>
 			</form>
