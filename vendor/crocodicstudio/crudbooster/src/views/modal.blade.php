5c95b1f8bd61a79b9eb311e88f3a0457f128fa64
Leandro Salvatti Piscke <piscke@gmail.com>
2018-03-13 21:32:57 -0300

    confirmar presença

---
+++ vendor/crocodicstudio/crudbooster/src/views/modal.blade.php
@@ -0,0 +1,18 @@
+<!-- Modal -->
+<div id="app">
+  <div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+  
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+            <inscricao :evento="3"></inscricao>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
