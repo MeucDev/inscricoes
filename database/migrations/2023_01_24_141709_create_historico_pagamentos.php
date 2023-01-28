87fc46ae13ce88010844de2abf486ff7ea56d39f
Allan Wiese <allanwiese@gmail.com>
2023-01-27 21:56:52 -0300

    fix: base de dados em prod não deixou criar FK na operação de criação da tabela; separado em dois steps

--- database/migrations/2023_01_24_141709_create_historico_pagamentos.php
+++ database/migrations/2023_01_24_141709_create_historico_pagamentos.php
@@ -17,11 +17,14 @@ class CreateHistoricoPagamentos extends Migration
             $table->increments('id');
             $table->string('operacao');
             $table->integer('inscricao_numero')->unsigned();
-            $table->foreign('inscricao_numero')->references('numero')->on('inscricoes');
             $table->float('valor');
             $table->string('pagseguro_code')->nullable();
 			$table->timestamps();
         });
+
+        Schema::table('historico_pagamentos', function (Blueprint $table) {
+            $table->foreign('inscricao_numero')->references('numero')->on('inscricoes');
+        }
     }
 
     /**
