845b2869dd6dbc803faee15933a6a10ec2fc875a
Allan Wiese <allanwiese@gmail.com>
2023-02-05 16:56:13 -0300

    fix: usando fork do laravel-pagseguro que contem ajuste para suportar PIX

--- app/PagSeguroNotificacao.php
+++ app/PagSeguroNotificacao.php
@@ -36,7 +36,7 @@ class PagSeguroNotificacao
 
         //3) Paga: a transação foi paga pelo comprador e o PagSeguro já recebeu uma confirmação da instituição financeira responsável pelo processamento.
         //4) Disponível: a transação foi paga e chegou ao final de seu prazo de liberação sem ter sido retornada e sem que haja nenhuma disputa aberta.
-        if ($info->getStatus()->getCode() == 3){
+        if ($info->getStatus()->getCode() == 3 || $info->getStatus()->getCode() == 4){
             print_r("Inscrição paga: " . $numero);
             $inscricao->inscricaoPaga = 1;
             $inscricao->valorInscricaoPago = $valor;
